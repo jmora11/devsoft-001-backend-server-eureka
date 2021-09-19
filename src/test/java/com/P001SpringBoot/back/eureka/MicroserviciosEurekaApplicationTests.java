b1622b04d79d1f0127d45504bc534bef185926a8
Julian Mora <juli.mora.5667@gmail.com>
2021-09-19 13:28:17 -0500

    complete dev

---
+++ src/test/java/com/P001SpringBoot/back/eureka/MicroserviciosEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P001SpringBoot.back.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
